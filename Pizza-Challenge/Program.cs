2e5afd8ddd3bb34737020162a19f0b8bcaeb165e
Leo Asare-Addo <22LAsareAddo@heckgrammar.co.uk>
2025-02-11 10:00:33 +0000

    Add project files.

---
+++ Pizza-Challenge/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Pizza_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] toppingsAvailable = new string[] { "Mushrooms", "Basil", "Chicken", "Olives", "Pepperoni", "Sausage" }
+            Console.WriteLine("How many toppings do you want?");
+            int toppingsWanted = Convert.ToInt32(Console.ReadLine());
+            int[]topping = new int[toppingsWanted];
+            for (int i = 0; i < toppingsWanted; i++)
+            {
+
+            }
+
+            //string[] toppings = new string[toppi];
+
+        }
+    }
+}
