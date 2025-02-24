d4b794d2e8612c4e025b2f3270be8fd8eeee6f4a
Leo Asare-Addo <22LAsareAddo@heckgrammar.co.uk>
2025-02-24 09:51:49 +0000

    d

--- Pizza-Challenge/Program.cs
+++ Pizza-Challenge/Program.cs
@@ -5,31 +5,37 @@ namespace Pizza_Challenge
     {
         static void Main(string[] args)
         {
-            toppings
+           string toppings;
             string[] toppingsAvailable = new string[] { "Mushrooms", "Basil", "Chicken", "Olives", "Pepperoni", "Sausage" }
             Console.WriteLine("How many toppings do you want?");
             int toppingsWanted = Convert.ToInt32(Console.ReadLine());
             int[]topping = new int[toppingsWanted];
             for (int i = 0; i < toppingsWanted; i++)
             {
-               Console.WriteLine("What topping do you want?")
-                   
-                   if (Console.ReadLine()).Contains(toppingsAvailable)
-                       toppings = Console.ReadLine()
+                Console.WriteLine("What topping do you want?"); 
+
+                if (Console.ReadLine())toppingsAvailable);
+                toppings = Console.ReadLine();
                      else
-                         Console.WriteLine("You have either added the same topping twice: choose another, or input an unavailable topping.")
+                    Console.WriteLine("You have either added the same topping twice: choose another, or input an unavailable topping.");
                     
                    
             }
-            char pizzaSize;
-            float smallPizza = 2.50
-            float medPizza = 4.50
-            float largePizza = 7.25
-            float toppingCost = 0.75
+            int pizzaSize;
+            float smallPizza = 2.50f;
+            float medPizza = 4.50f;
+            float largePizza = 7.25f;
+            float toppingCost = 0.75f;
             float cost;
             Console.Write("What size pizza do you want? (S for small, M for medium, L for large)")
-            cost = (toppingCost*toppingsWanted) + pizzaSize
-                Console.WriteLine("The cost is: ") + cost
+            pizzaSize = Convert.ToInt32(Console.ReadLine());
+            if (pizzaSize == 'S') {
+                pizzaSize = Convert.ToInt32(smallPizza)
+                    
+                    }
+
+            cost = (toppingCost * toppingsWanted) + pizzaSize;
+               Console.WriteLine("The cost is: ") + cost;
 
                 
 
